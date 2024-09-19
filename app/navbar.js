1830136d6620bf0033455210060179d6a6dcbe17
Lee Richardson <lee.richardson4@gmail.com>
2024-09-19 17:48:16 +0200

    quick site
    quick site first commit

---
+++ app/navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+      <nav className="navigation">
+        <ul>
+          <li><a className="navLink" href="mailto:hejleerichardson.dk">Email</a></li>
+          <li><a className="navLink" href="http://www.linkedin.com/in/lee-richardson-ux-designer">LinkedIn</a></li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
