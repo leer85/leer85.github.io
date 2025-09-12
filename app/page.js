f4dd76d8066cf7ae5cbe18c34a1843add682d8b4
Lee <lee@LMA.local>
2025-09-12 09:01:53 +0200

    changes to text and style

--- app/page.js
+++ app/page.js
@@ -4,21 +4,19 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.grid}>
-        <h1>Product designer in Copenhagen.</h1>
+        <h1>Lee Richardson</h1>
       </div>
       
       <div className={styles.textGrid}>
-        <p>I'm a product designer based in Copenhagen, currently studying software design at ITU. I specialize in solving business challenges through innovative design, visual identity development and digital transformation strategies.</p>
+        <p>I'm a product designer in Copenhagen, currently studying <span className="bodyLink"><a href="https://en.itu.dk/Programmes/MSc-Programmes/Software-Design">software design</a></span> at ITU. I specialize in solving business challenges through innovative design, visual identity development and digital transformation strategies.</p>
 
         <p>In my design approach, every project begins with exploratory dialogue to identify client needs, followed by research and strategic planning. I am able to handle the entire design process from concept to execution, and use measurable methodologies to deliver appropriate and lasting solutions.</p>
 
-        <p>I manage the entire design process from concept to execution, applying methodologies to deliver appropriate and lasting solutions.</p>
+        <p>While at Hero Gaming I was UX specialist, delivering measurable results including <span className="bodyLink"><a href="https://www.figma.com/proto/pmOcYC7bkDepWrFEfW9GeE/cv-2025?node-id=96-132&t=a62Y9FN6FSQqTJpU-1">creating a feature</a></span> that increased user engagement by 44%, a <span className="bodyLink"><a href="https://www.figma.com/proto/pmOcYC7bkDepWrFEfW9GeE/cv-2025?node-id=70-403&t=a62Y9FN6FSQqTJpU-1">signup flow optimization</a></span> that boosted conversions by 73%, and a <span className="bodyLink"><a href="https://www.figma.com/proto/pmOcYC7bkDepWrFEfW9GeE/cv-2025?node-id=98-96&t=oGMLAQElPD725VVE-1">landing page redesign</a></span> that improved NPS scores from 59 to 72.</p>
 
-        <p>While at Hero Gaming I was UX specialist, delivering measurable results including <span className="bodyLink"><a href="https://www.figma.com/proto/pmOcYC7bkDepWrFEfW9GeE/cv-2025?node-id=96-132&t=a62Y9FN6FSQqTJpU-1">a feature</a></span> that increased user engagement by 44%, a <span className="bodyLink"><a href="https://www.figma.com/proto/pmOcYC7bkDepWrFEfW9GeE/cv-2025?node-id=70-403&t=a62Y9FN6FSQqTJpU-1">signup flow optimization</a></span> that boosted conversions by 73%, and a landing page redesign (http://localhost:3000/)  that improved NPS scores from 59 to 72.</p>
+        <p>For some years I ran a UX consultancy business, mainly focusing on optimizating e-commerce flows. My 2023 <span className="bodyLink"><a href="https://www.figma.com/proto/pmOcYC7bkDepWrFEfW9GeE/cv-2025?node-id=70-263&t=a62Y9FN6FSQqTJpU-1">project</a></span> with Beklaedt achieved a 48% checkout conversion rate, exceeding the 30% target.</p>
 
-        <p>For some years I ran a UX consultancy business, focusing on e-commerce optimization. My 2023 <span className="bodyLink"><a href="https://www.figma.com/proto/pmOcYC7bkDepWrFEfW9GeE/cv-2025?node-id=70-263&t=a62Y9FN6FSQqTJpU-1">project</a></span> with Beklaedt achieved a 48% checkout conversion rate, exceeding the 30% target.</p>
-
-        <p>As part of Shopify's internal innovation team, I designed an <span className="bodyLink"><a href="https://www.figma.com/proto/pmOcYC7bkDepWrFEfW9GeE/cv-2025?node-id=70-157&t=a62Y9FN6FSQqTJpU-1">onboarding experience</a></span> that reduced new user bounce rates by 42%.</p>
+        <p>As part of Shopify's internal innovation team, I designed an <span className="bodyLink"><a href="https://www.figma.com/proto/pmOcYC7bkDepWrFEfW9GeE/cv-2025?node-id=70-157&t=a62Y9FN6FSQqTJpU-1">onboarding experience</a></span> that reduced bounce rates in new users by 42%.</p>
 
         <p>I contributed UX flows and refactored JavaScript for the Toronto Star's digital platforms.</p>
 
